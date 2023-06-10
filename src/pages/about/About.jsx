0bda7fcad482fdb8a1fbda750a72f178a0b028a3
Chrispy <christiancarteno@hotmail.com>
2023-06-09 23:25:48 -0700

    added footer and added to the about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import Logo from '../../images/logo55.png'
 
 import './about.css'
 
 const About = () => {
   return (
-    <div>
-      <br/>
-      <br/>
-      <h1> U8 GAMERS </h1>
-      this is the about page
-      <p className="something">this is something</p>
-    </div>
+    <header className="main__header">
+      <div className="container main__header-container">
+        <div className="main__header-left">
+          <h4>#GetGooeyWithIt</h4>
+          <h1>Join The Legends Of The Gaming World</h1>
+          <p>
+            Pum Da Da Da Dun We are gamers
+          </p>
+          <Link to="/plans" className='btn lg'> Get Started </Link>
+        </div>
+        <div className="main__header-right">
+          <div className="main__header-circle"></div>
+          <div className="main__header-image">
+            <img src={Logo} alt="Main Header" />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
