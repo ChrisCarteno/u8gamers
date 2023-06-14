272d4590566ddd8e80bbcf3da6a6ff62644b626f
Chrispy <christiancarteno@hotmail.com>
2023-06-13 20:20:31 -0700

    added images and played with Roster file

--- src/pages/roster/Roster.jsx
+++ src/pages/roster/Roster.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-
 import './roster.css'
+
+import FrannyImg from '../../images/Franny.png'
+import GoonerImage from '../../images/GoonerRIkcy.png'
+import JoelTank from '../../images/JoelTank.png'
+import NateDog from '../../images/NateDog.jpg'
+import PabloMinerals from '../../images/PabloMinerals.png'
+import PETERDAYS from '../../images/PETERDAYS.jpg'
+import ROACHATA from '../../images/ROACHATA.jpg'
+import u8CHuy from '../../images/u8CHuy.png'
 
 class CardHeader extends React.Component {
   render() {
     const { image } = this.props;
     var style = { 
-        backgroundImage: 'url(' + image + ')',
+        backgroundImage: 'url(' + "../images/Franny.png" + ')',
     };
     return (
       <header style={style} id={image} className="card-header">
         <h4 className="card-header--title">News</h4>
+        <img src={FrannyImg} alt="Logo" />
       </header>
     )
   }
@@ -46,7 +55,7 @@ class Card extends React.Component {
   render() {
     return (
       <article className="cardGamer">
-        <CardHeader image={''}/>
+        <CardHeader image=''/>
         <CardBody signDate={"March 20 2015"} title={'Ronky Kong'} text={'Ronky Is one of the greatest smash players in Coachella Valley Scene'}/>
       </article>
     )
