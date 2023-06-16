04a28956dcd14214019f348b2f5794576ee40cb3
Chrispy <christiancarteno@hotmail.com>
2023-06-15 22:12:18 -0700

    adding styling to search bar component

--- src/components/Flexbox.jsx
+++ src/components/Flexbox.jsx
@@ -4,11 +4,7 @@ import "./flexbox.css";
 const Flexbox = () => {
     return(
         <div className="flexbox">
-            <div className="flexbox__item">1</div>
-            <div className="flexbox__item">2</div>
-            <div className="flexbox__item">3</div>
-            <div className="flexbox__item">4</div>
-            <div className="flexbox__item">5</div>
+            
         </div>
     )
 }
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
 import './shop.css'
+import Flexbox from '../../components/Flexbox'
 
 const Shop = () => {
   return (
-    <div>Shop</div>
-  )
+    <header className="main__header">
+      <div className="container main__header-container">
+        <h1>Shop</h1>
+        <Flexbox />    
+      </div>
+    </header>
+    )
 }
 
 export default Shop
