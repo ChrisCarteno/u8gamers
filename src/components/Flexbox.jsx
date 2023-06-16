751fb33eb69d48be4822c23c6420812ece2538e5
Chrispy <christiancarteno@hotmail.com>
2023-06-15 22:00:54 -0700

    adding containers

---
+++ src/components/Flexbox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./flexbox.css";
+
+const Flexbox = () => {
+    return(
+        <div className="flexbox">
+            <div className="flexbox__item">1</div>
+            <div className="flexbox__item">2</div>
+            <div className="flexbox__item">3</div>
+            <div className="flexbox__item">4</div>
+            <div className="flexbox__item">5</div>
+        </div>
+    )
+}
+
+export default Flexbox;
