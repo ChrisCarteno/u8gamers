71c408154b4184154babfa0f5bc96f2302cc9889
Chrispy <christiancarteno@hotmail.com>
2023-06-16 22:52:23 -0700

    added some copy to news page, need to make it look nice still

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -9,6 +9,25 @@ const News = () => {
     <header className="main__header">
       <h1>Find Out The Latest Happening With U8</h1>
       
+Introducing u8 Gamers: Unleash the Power of Your Play!
+
+Are you ready to take your gaming skills to the next level? Look no further than u8 Gamers, the premier gaming organization dedicated to fueling your passion and unlocking your true gaming potential. Get ready to immerse yourself in a world of unparalleled excitement, thrilling competition, and limitless possibilities.
+
+Why choose u8 Gamers? Here's why:
+
+Elevate Your Game: At u8 Gamers, we believe in pushing the boundaries of what's possible in gaming. Whether you're an aspiring pro gamer or a casual enthusiast, our team of experienced coaches and mentors will help you refine your skills, develop winning strategies, and reach new heights of achievement.
+
+Join a Thriving Community: Gaming is more than just playing—it's about connecting with like-minded individuals who share your passion. When you become part of the u8 Gamers community, you'll find a vibrant and welcoming group of fellow gamers who will become your teammates, friends, and lifelong companions on your gaming journey.
+
+Embrace Endless Opportunities: u8 Gamers opens doors to exciting opportunities in the gaming world. As a member, you'll gain access to exclusive tournaments, leagues, and events where you can showcase your talents and compete against top-tier gamers. Who knows? You might catch the eye of sponsors or professional teams scouting for rising stars.
+
+Professional Support at Your Fingertips: We understand the challenges that gamers face, from technical issues to mental fatigue. That's why u8 Gamers provides comprehensive support services to ensure you can focus on what you do best—gaming. Our dedicated team is available round-the-clock to assist you with technical queries, mental well-being, and any other concerns you may have.
+
+Unleash Your Creativity: Gaming is not just about winning; it's a canvas for your creativity. At u8 Gamers, we celebrate your unique style and help you express yourself through content creation, streaming, and esports journalism. Unleash your creativity and become a respected influencer in the gaming community.
+
+Join u8 Gamers today and embark on a thrilling gaming adventure that will redefine your gaming experience. Together, we'll conquer new challenges, forge unforgettable friendships, and create gaming memories that will last a lifetime. Get ready to level up with u8 Gamers—the ultimate gaming organization for champions like you!
+
+Visit our website at www.u8gamers.com and discover the power of play like never before. Let's #GameOnTogether!
     </header>
   )
 }
