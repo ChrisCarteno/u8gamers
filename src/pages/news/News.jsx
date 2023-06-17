aecab999a5de3be99a82aed823433b50401f8e42
Chrispy <christiancarteno@hotmail.com>
2023-06-16 22:50:52 -0700

    working on adding content to news page

--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../images/logo55.png'
 
 import './news.css'
 
 const News = () => {
   return (
-    <div>News</div>
+    <header className="main__header">
+      <h1>Find Out The Latest Happening With U8</h1>
+      
+    </header>
   )
 }
 
