07cdc2d7dbb41d9db3dec13c363346baca76e03a
Chrispy <christiancarteno@hotmail.com>
2023-06-17 12:01:27 -0700

    adding an event commponent

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
-
 import './events.css'
 
-const Events = () => {
+const Events = ({ title, date, location, description }) => {
   return (
-    <div>Events</div>
+  <header className="main__header">
+    <div className="event-card">
+    <h3>{title}</h3>
+    <p>Date: {date}</p>
+    <p>Location: {location}</p>
+    <p>{description}</p>
+  </div>
+  </header>
   )
 }
+
+// EventsPage component to display the events page content
+class EventsPage extends React.Component {
+  state = {
+    events: [
+      {
+        title: 'Gaming Tournament Extravaganza',
+        date: 'July 15, 2023',
+        location: 'Virtual Arena',
+        description: 'Join us for an epic gaming tournament featuring top players from around the world!'
+      },
+      {
+        title: 'Game Developers Conference',
+        date: 'August 22-24, 2023',
+        location: 'Convention Center',
+        description: 'Explore the latest trends and innovations in game development with industry experts.'
+      },
+      // Add more events as needed
+    ]
+  };
+
+  render() {
+    const { events } = this.state;
+
+    return (
+      <div className="events-page">
+        <h2>Upcoming Events</h2>
+        {events.length > 0 ? (
+          events.map((event, index) => (
+            <Events
+              key={index}
+              title={event.title}
+              date={event.date}
+              location={event.location}
+              description={event.description}
+            />
+          ))
+        ) : (
+          <p>No upcoming events at the moment. Please check back regularly for updates!</p>
+        )}
+      </div>
+    );
+  }
+}
+
 
 export default Events
--- src/pages/news/News.jsx
+++ src/pages/news/News.jsx
@@ -6,27 +6,25 @@ import './news.css'
 
 const News = () => {
   return (
-    <div>
-      <header className="main__header">
-        <h1>Find Out The Latest Happening With U8</h1>
-        <h3>
-          Elevate Your Game: At u8 Gamers, we believe in pushing the boundaries of what's possible in gaming. Whether you're an aspiring pro gamer or a casual enthusiast, our team of experienced coaches and mentors will help you refine your skills, develop winning strategies, and reach new heights of achievement.
-          <br/>
-          Join a Thriving Community: Gaming is more than just playing—it's about connecting with like-minded individuals who share your passion. When you become part of the u8 Gamers community, you'll find a vibrant and welcoming group of fellow gamers who will become your teammates, friends, and lifelong companions on your gaming journey.
-          <br/>
-          Embrace Endless Opportunities: u8 Gamers opens doors to exciting opportunities in the gaming world. As a member, you'll gain access to exclusive tournaments, leagues, and events where you can showcase your talents and compete against top-tier gamers. Who knows? You might catch the eye of sponsors or professional teams scouting for rising stars.
-          <br/>
-          Professional Support at Your Fingertips: We understand the challenges that gamers face, from technical issues to mental fatigue. That's why u8 Gamers provides comprehensive support services to ensure you can focus on what you do best—gaming. Our dedicated team is available round-the-clock to assist you with technical queries, mental well-being, and any other concerns you may have.
-          <br/>
-          Unleash Your Creativity: Gaming is not just about winning; it's a canvas for your creativity. At u8 Gamers, we celebrate your unique style and help you express yourself through content creation, streaming, and esports journalism. Unleash your creativity and become a respected influencer in the gaming community.
-          <br/>
-          Join u8 Gamers today and embark on a thrilling gaming adventure that will redefine your gaming experience. Together, we'll conquer new challenges, forge unforgettable friendships, and create gaming memories that will last a lifetime. Get ready to level up with u8 Gamers—the ultimate gaming organization for champions like you!
-          <br/>
-          Visit our website at <a href='www.u8gamers.com' alt="u8gamers">www.u8gamers.com</a> and discover the power of play like never before. Let's #GameOnTogether!
-        </h3>
-      </header>
-
+    <header className="main__header">
+    <div className="container main__header-container">
+      <div className="main__header-left">
+        <h4>#GetGooeyWithIt</h4>
+        <h1>Join The Legends Of The Gaming World</h1>
+        <Link to="/shop" className='btn lg'> Shop </Link>
+        <Link to="/esports" className='btn lg'> Roster </Link>
+        <Link to="/events" className='btn lg'> Events </Link>
+        <Link to="/news" className='btn lg'> News </Link>
+        <br></br>
+      </div>
+      <div className="main__header-right">
+        <div className="main__header-circle"></div>
+        <div className="main__header-image">
+          <img src={Logo} alt="Main Header" />
+        </div>
+      </div>
     </div>
+  </header>
   )
 }
 
