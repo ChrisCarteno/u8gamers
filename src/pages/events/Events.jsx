7253637b246e04f03a348613c46984debdedff87
chrispy <christiancarteno@hotmail.com>
2023-07-22 21:47:08 -0700

    removed unused code, replaced dummy text

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -10,22 +10,19 @@ const Events = () => {
       id: 0,
       title: 'Barbenheimer: Movie Day',
       image: "https://agentsoffandom.com/wp-content/uploads/2023/07/IMG_6060-819x1024.jpg",
-      icon: BsFilm,
       date: 'June 26, 2023'
     },
     {
       id: 1,
-      title: 'Barbenheimer: Movie Day',
-      image: "https://agentsoffandom.com/wp-content/uploads/2023/07/IMG_6060-819x1024.jpg",
-      icon: BsFilm,
+      title: 'Halloween Spooktacular',
+      image: "",
       date: 'June 26, 2023'
     },
     {
       id: 2,
-      title: 'Barbenheimer: Movie Day',
-      image: "https://agentsoffandom.com/wp-content/uploads/2023/07/IMG_6060-819x1024.jpg",
-      icon: BsFilm,
-      date: 'June 26, 2023'
+      title: 'Big Bear Summit',
+      image: "",
+      date: 'January 26, 2024'
     }
   ];
 
@@ -37,7 +34,7 @@ const Events = () => {
         <MultiActionAreaCard key={'event' + article.id} title={article.title} author={article.author} date={article.date} icon={article.icon}/>
       ))}
       </div>
-      
+
     </div>
   )
 }
