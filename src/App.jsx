181a10d6bc7300a3399c8a35ac1fbb3fc0e7a56f
Chrispy <christiancarteno@hotmail.com>
2023-08-01 22:55:11 -0700

    getting started on gamer's profiles

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import News from './pages/news/News'
 import Roster from './pages/roster/Roster'
 import Shop from './pages/shop/Shop'
 import NewsArticle from './components/NewsArticle'
+import Gamer from './components/Gamer'
 
 import Footer from './components/Footer';
 import NavBar from "./components/NavBar";
@@ -22,6 +23,7 @@ function App() {
         <Route path="/events" element={<Events/>} />
         <Route path="/news" element={<News/>} />
         <Route path="/roster" element={<Roster/>} />
+        <Route path="/roster/:gamerId" element={<Gamer />} />
         <Route path="/shop" element={<Shop/>} />
         <Route path="/news/:newsId" element={<NewsArticle/>} />
       </Routes>
---
+++ src/components/Gamer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gamer = () => {
+  return (
+    <div>Gamer</div>
+  )
+}
+
+export default Gamer
