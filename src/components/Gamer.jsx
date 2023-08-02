f777216dfcf00011e406a374231454c6416cad0b
Chrispy <christiancarteno@hotmail.com>
2023-08-02 01:45:11 -0700

    adding to the gamer pages profiles

--- src/components/Gamer.jsx
+++ src/components/Gamer.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './gamer.css'; // Import CSS file for styling
+import { useParams } from 'react-router-dom';
 
-const Gamer = () => {
+const GamerProfile = () => {
+  const profiles = [
+    {
+      id: 1,
+    }
+  ];
+
+  let profilesArr = profiles[0].content.split('.');
+  console.log(profilesArr);
+
+  const params = useParams();
+  let { newsId } = params;
+
   return (
-    <div>Gamer</div>
-  )
-}
+    <div className="gamer">
+      <h1 className='gamer__title'>{profiles[newsId].title}</h1>
+      <p className="gamerContent"> {profiles[newsId].title}</p>
+      <p className="gamer-info">
+        By <span className="gamer-author">{profiles[newsId].title}</span> | {profiles[newsId].date} 
+      </p>
 
-export default Gamer
+    </div>
+  );
+};
+
+export default GamerProfile;
