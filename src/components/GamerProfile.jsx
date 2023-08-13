b5fc047f9255e17eff09feee111c0caf989a6d47
Chrispy <christiancarteno@hotmail.com>
2023-08-12 21:49:37 -0700

    added images and text some styling is left to do

--- src/components/GamerProfile.jsx
+++ src/components/GamerProfile.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './gamer.css'; // Import CSS file for styling
 import { useParams } from 'react-router-dom';
+import FrannyImg from '../images/Franny.png'
+import GoonerImage from '../images/GoonerRIkcy.png'
+import JoelTank from '../images/JoelTank.png'
+import uCHuy from '../images/u8CHuy.png'
+import PabloMinerals from '../images/PabloMinerals.png'
+import PETERDAYS from '../images/PETERDAYS.jpg'
+import ROACHATA from '../images/ROACHATA.jpg'
+import NateDog from '../images/NateDog.jpg'
 
 const GamerProfile = () => {
   const articles = [
@@ -8,49 +16,57 @@ const GamerProfile = () => {
       id: 0,
       gamer: 'Franny Duran',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: FrannyImg
     },
     {
       id: 1,
       gamer: 'Gooner',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: GoonerImage
     },
     {
       id: 2,
       gamer: 'Joel',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: JoelTank
     },
     {
       id: 3,
       gamer: 'Chuy',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: uCHuy
     },
     {
       id: 4,
       gamer: 'Pablo',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: PabloMinerals
     },
     {
       id: 5,
       gamer: 'Peter',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: PETERDAYS
     },
     {
       id: 6,
       gamer: 'Roach',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: ROACHATA
     },
     {
       id: 7,
       gamer: 'Nate',
       excerpt: `I am a gamer through and through`,
-      joindate: 'June 26, 2023'
+      joindate: 'June 26, 2023',
+      image: NateDog
     }
     
   ];
@@ -86,14 +102,16 @@ const GamerProfile = () => {
       gamerId = 0;
   } 
   return (
-    <div className="main__news">
-    <div className="article">
+    <div className="">
+    <div className="gamer">
       <h1 className='news__title'>{articles[gamerId].gamer}</h1>
-      <p className="articleContent"> {articles[gamerId].excerpt}</p>
+      <div className="">
+        <img style={{width: "50%"}} src={articles[gamerId].image} alt="Logoooo"/>
+       <p className="articleContent"> {articles[gamerId].excerpt}</p>
+      </div>
       <p className="article-info">
         <span className="article-author">DATE JOINED</span> | {articles[gamerId].joindate} 
       </p>
-
     </div>
     </div>
   );
